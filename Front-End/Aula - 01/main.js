0d3e6cfdebc6dbc30733a8e644de52bdc898c052
Erymanthusn <kaique.carvalho.costa8@gmail.com>
2026-02-02 16:48:38 -0300

    Add color change buttons and update functionality

--- Front-End/Aula - 01/main.js
+++ Front-End/Aula - 01/main.js
@@ -1,10 +1,43 @@
 'use strict'
 
 const botaoTrocaCor = document.getElementById('botao-Troca-Cor')
+const botaoVerde = document.getElementById('botao-Verde')
+const botaoVermelho = document.getElementById('botao-Vermelho')
 
 function trocarCor(){
-    const corUsuario = document.getElementById('cor-Usuario').value.toLowerCase().trim()
-    document.documentElement.style.setProperty('--color-bg', corUsuario)
+
+let cor = document.getElementById('cor').value
+if(cor == 'vermelho'){
+    cor = 'Red'
+}else if( cor == 'verde'){
+    cor = "Green"
+}else if( cor == 'azul'){
+    cor = "Blue"
+}else if( cor == 'roxo'){
+    cor = "Purple"
+}
+
+if (botaoVerde == true) {
+    document.documentElement.style.setProperty('--color-bg', 'green')
+} else if (botaoVermelho == true){
+    document.documentElement.style.setProperty('--color-bg', 'red')
+}
+
+    document.documentElement.style.setProperty('--color-bg', cor)
+
+}
+
+function trocarCorVermelho(){
+    
+     document.documentElement.style.setProperty('--color-bg', 'red')
+    
+}
+function trocarCorVerde(){
+    
+    document.documentElement.style.setProperty('--color-bg', 'green')
+   
 }
 
 botaoTrocaCor.addEventListener('click', trocarCor)
+botaoVerde.addEventListener('click', trocarCorVerde)
+botaoVermelho.addEventListener('click', trocarCorVermelho)
